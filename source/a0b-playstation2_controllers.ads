ed645876f39336a6afb7d46709c5aff758066c7c
Vadim Godunko <vgodunko@gmail.com>
2024-02-25 22:24:43 +0400

    All buttons and joysticks.

--- source/a0b-playstation2_controllers.ads
+++ source/a0b-playstation2_controllers.ads
@@ -19,7 +19,42 @@ package A0B.PlayStation2_Controllers is
    pragma Pure;
 
    type Controller_State is record
-      null;
+      Right_Joystick_Horizontal : A0B.Types.Unsigned_8;  --  Analog
+      Right_Joystick_Vertical   : A0B.Types.Unsigned_8;  --  Analog
+      Left_Joystick_Horizontal  : A0B.Types.Unsigned_8;  --  Analog
+      Left_Joystick_Vertical    : A0B.Types.Unsigned_8;  --  Analog
+
+      Right_Direction_Button    : A0B.Types.Unsigned_8;  --  Digital/Analog
+      Left_Direction_Button     : A0B.Types.Unsigned_8;  --  Digital/Analog
+      Up_Direction_Button       : A0B.Types.Unsigned_8;  --  Digital/Analog
+      Down_Direction_Button     : A0B.Types.Unsigned_8;  --  Digital/Analog
+
+      Triangle_Button           : A0B.Types.Unsigned_8;  --  Digital/Analog, Y
+      Circle_Button             : A0B.Types.Unsigned_8;  --  Digital/Analog, B
+      Cross_Button              : A0B.Types.Unsigned_8;  --  Digital/Analog, A
+      Square_Button             : A0B.Types.Unsigned_8;  --  Digital/Analog, X
+
+      Left_1_Button             : A0B.Types.Unsigned_8;  --  Digital/Analog
+      Right_1_Button            : A0B.Types.Unsigned_8;  --  Digital/Analog
+      Left_2_Button             : A0B.Types.Unsigned_8;  --  Digital/Analog
+      Right_2_Button            : A0B.Types.Unsigned_8;  --  Digital/Analog
+
+      Right_Joystick            : A0B.Types.Unsigned_8;  --  Digital
+      Left_Joystick             : A0B.Types.Unsigned_8;  --  Digital
+      Select_Button             : A0B.Types.Unsigned_8;  --  Digital
+      Start_Button              : A0B.Types.Unsigned_8;  --  Digital
    end record;
+   --  Analog joysticks components has range 16#00# .. 16#FF# with neutral
+   --  value at 16#80#.
+   --
+   --  Digital/analog buttons in analog mode has value in range 16#00#..16#FF#,
+   --  where 16#00# means that button is released, and other values is value
+   --  of pressure.
+   --
+   --  Digital/analog buttons in digital mode has only 16#00# or 16#FF# value,
+   --  where 16#00# means that button is released and 16#FF# value means that
+   --  button is pressed.
+   --
+   --  Digital buttons has only 16#00# or 16#FF# value.
 
 end A0B.PlayStation2_Controllers;
